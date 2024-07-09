9a86b1816792bc2391a9024b99d1111cdfca17f0
Ankit Kumar <32966324+ankit2409@users.noreply.github.com>
2024-07-09 13:54:58 +0530

    Update esp32_elan_code_prefrences_multithreaded.ino

--- esp32_elan_code_prefrences_multithreaded.ino
+++ esp32_elan_code_prefrences_multithreaded.ino
@@ -13,8 +13,9 @@
 #include <LiquidCrystal_I2C.h>
 // #include <NTPClient.h>
 // #include <WiFiUdp.h>
-#include <Preferences.h>
-#include <time.h>
+#include<Preferences.h>
+#include<time.h>
+
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
@@ -29,8 +30,15 @@ const long utcOffsetInSeconds = 19800; // UTC offset for Indian Standard Time (I
 
 //Set LED pin as GPIO5
 /****** WiFi Connection Details *******/
-const char* ssid = "ankit";
-const char* password = "anki1234";
+// const char* ssid = "";
+// const char* password = "";
+// const char* backupSsid = "ankit" ;
+// const char* backupPassword = "anki1234";
+
+String ssid = "";
+String password = "";
+const char* backupSsid = "ankit" ;
+const char* backupPassword = "anki1234";
 
 unsigned int count;
 unsigned int mqqtLastCount = 0;
@@ -39,10 +47,27 @@ unsigned int nvsLastCount = 0;
 // unsigned long epochTime;
 
 int sensorPin = 5;
+
+String company = "XYZ";
 String machineSiteId = "HDR-01";
+
+
 const char* machineTopic = "PMP_data_test_HDR-01";
+const char* configWiFiTopicPrecede = "config/update/wifi/";
+const char* configMqttTopicPrecede = "config/update/mqtt/";
+const char* configCompanyTopicPrecede = "config/update/company/";
+const char* configResetCounter = "config/update/counter/";
+const char* publishMsgTopicPrecede = "message/";
+
+String resultMqttTopic = "";
+String resultWiFiTopic = "";
+String resultCompanyTopic = "";
+String resultPublishTopic = "";
+String resultCounterResetTopic = "";
 
 const char* wifiStatus = "WiFi NA";
+
+String deviceId = "";
 
 
 
@@ -53,7 +78,7 @@ LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars
 
 /******* MQTT Broker Connection Details *******/
 const char* mqtt_server = " d08900cfdef5463098201f44a1532917.s2.eu.hivemq.cloud";
-const char* mqtt_username = "ankit1";
+const char* mqtt_username = "ankit";
 const char* mqtt_password = "Ankit@123";
 const int mqtt_port =8883;
 
@@ -69,6 +94,7 @@ const unsigned long interval = 60000;  // 1 minute in milliseconds
 unsigned long lastMinuteMillis = 0; // Time of the last minute
 int count1=0;
 int feed=0;
+unsigned long last_mqtt_send_time = 0;
 
 /**** MQTT Client Initialisation Using WiFi Connection *****/
 PubSubClient client(espClient);
@@ -81,36 +107,59 @@ const unsigned long mqtt_send_interval = 5000;
 DynamicJsonDocument doc(1024);
 
 
+
 void setup_wifi() {
   delay(10);
-  Serial.print("\nConnecting to ");
-  Serial.println(ssid);
+
+  ssid = preferences.getString("SSID",backupSsid); 
+  password = preferences.getString("PASSWORD",backupPassword);
+
+  bool usingBackup = false; // Flag to check if backup password is being used
+
+  if (ssid == backupSsid && password == backupPassword) {
+    usingBackup = true;
+  }
+
+  
 
   WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
+  WiFi.begin(ssid.c_str(), password.c_str());
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(300);
-    Serial.print(".");
+  if (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    wifiStatus = "WiFi NA";
+    Serial.println("WiFi NA.........");
   }
+  
   randomSeed(micros());
-  Serial.println("\nWiFi connected\nIP address: ");
-  wifiStatus = "WiFi Conn..";
-  Serial.println(WiFi.localIP());
+  if(WiFi.status() == WL_CONNECTED){
+    Serial.println("\nWiFi connected IP address: ");
+    Serial.print("\nConnecting to ");
+    Serial.println(ssid+"...   "+password);
+    wifiStatus = "WiFi Conn..";
+    Serial.println(WiFi.localIP());
+    if (usingBackup) {
+    Serial.println("Connected using backup SSID and password");
+    } else {
+      Serial.println("Connected using stored SSID and password");
+    }
+  }  
+
 }
-
-
 void reconnect() {
   // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    String clientId = "ESP8266Client-";   // Create a random client ID
+  if (!client.connected()) {
+    Serial.println("Attempting MQTT connection...");
+    String clientId = "MQTT_FX_Client";   // Create a random client ID
     clientId += String(random(0xffff), HEX);
     // Attempt to connect
     if (client.connect(clientId.c_str(), mqtt_username, mqtt_password)) {
-      Serial.println("connected");
-
-      client.subscribe(machineTopic);   // subscribe the topics here
+      Serial.println("connected to Mqtt");
+      
+      client.subscribe(configMqttTopicPrecede);
+      client.subscribe(configWiFiTopicPrecede);
+      client.subscribe(configResetCounter);
+      // break;
   
     } else {
       Serial.print("failed, rc=");
@@ -120,8 +169,37 @@ void reconnect() {
     }
   }
 }
+void callback(char* topic, byte* payload, unsigned int length) {
+  String incommingMessage = "";
+  for (int i = 0; i < length; i++) incommingMessage+=(char)payload[i];
 
+  Serial.println("Message arrived ["+String(topic)+"]"+incommingMessage);
 
+  if(String(topic) == resultWiFiTopic){
+    DynamicJsonDocument wifidoc(1024);
+    deserializeJson(wifidoc, incommingMessage);
+    ssid = wifidoc["ssid"].as<String>();
+    password = wifidoc["password"].as<String>();
+    preferences.putString("SSID", ssid); 
+    preferences.putString("PASSWORD", password);  
+    setup_wifi();
+  }
+
+  else if(String(topic) == resultMqttTopic){
+    DynamicJsonDocument mqttdoc(1024);
+    deserializeJson(doc, incommingMessage);
+    mqtt_server = mqttdoc["mqttServer"];
+    mqtt_username = mqttdoc["mqttUsername"];
+    mqtt_password = mqttdoc["mqttPassword"];
+    reconnect();
+
+  }
+
+  else if(String(topic) == resultCounterResetTopic){
+    count = 0;
+    preferences.putUInt("counter", count);
+  }
+}
 void publishMessage(const char* topic, String payload , boolean retained){
   if(!client.connected())
     reconnect();
@@ -131,25 +209,50 @@ void publishMessage(const char* topic, String payload , boolean retained){
 void setup() {
 
   Serial.begin(115200);
+  preferences.begin("my-app", false);   // initialize perfernces
   while (!Serial) delay(1);
+  setup_wifi();
+  
+  deviceId = WiFi.macAddress();
+
+  resultMqttTopic  = String(configMqttTopicPrecede) + deviceId;
+  resultWiFiTopic = String(configWiFiTopicPrecede) + deviceId;
+  resultCompanyTopic = String(configCompanyTopicPrecede) + deviceId;
+  resultPublishTopic = String(publishMsgTopicPrecede) + company + "/" + deviceId;
+  resultCounterResetTopic = String(configResetCounter) +  deviceId;
+
+
+  configMqttTopicPrecede = resultMqttTopic.c_str();
+  configWiFiTopicPrecede = resultWiFiTopic.c_str();
+  configCompanyTopicPrecede = resultCompanyTopic.c_str();
+  publishMsgTopicPrecede = resultPublishTopic.c_str();
+  configResetCounter = resultCounterResetTopic.c_str();
+
+
+
+  Serial.println(configMqttTopicPrecede);
+  Serial.println(configWiFiTopicPrecede);
+  Serial.println(configCompanyTopicPrecede);
+  Serial.println(publishMsgTopicPrecede);
+
+  
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer); //configure time 
   
 
   espClient.setInsecure();
  
   client.setServer(mqtt_server, mqtt_port);
-  
-  client.subscribe(machineTopic);
+  client.setCallback(callback);
   client.setKeepAlive(MQTT_KEEP_ALIVE_INTERVAL);
+  reconnect();
+  
   count = 0 ;
 
   lcd.init();                      // initialize the lcd 
   // Print a message to the LCD.
   lcd.backlight();
   lcd.setCursor(0,0);
-  lcd.print(machineSiteId);
-
-  preferences.begin("my-app", false);   // initialize perfernces
-
+  // lcd.print(machineSiteId);
 
   xTaskCreatePinnedToCore(
       publishMqttMessage, // Task function
@@ -161,16 +264,55 @@ void setup() {
       0 // Core to run the task on (0 or 1)
     );
 }
-
 void loop() {
-  lcd.setCursor(0,0);
-  lcd.print(machineSiteId);
+  // lcd.clear();
+  // lcd.setCursor(0,0);
+  // lcd.print(machineSiteId);
   // lcd.setCursor(7,0);
   // lcd.print(&timeinfo, "%H:%M:%S");
-  lcd.setCursor(7,1);
-  lcd.print(wifiStatus);
+  // lcd.setCursor(7,1);
+  // lcd.print(wifiStatus);
+  // Serial.println(wifiStatus);
+  // Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+    // Serial.println(" count from another thread is "+ String(count)+" mqqtLastCount is "+String(mqqtLastCount));
+
+    if (WiFi.status() != WL_CONNECTED) {
+      // lcd.clear();
+      wifiStatus = "WiFi NA";
+      // Serial.println("Not connected to Wi-Fi. Reconnecting...");
+      setup_wifi(); // Reconnect to Wi-Fi
+      configTime(gmtOffset_sec, daylightOffset_sec, ntpServer); //configure time 
+      reconnect(); // Reconnect to MQTT broker
+    }
+    if(!getLocalTime(&timeinfo)){
+    // Serial.println("Failed to obtain time");
+    return;
+    }
+    client.loop();
   
-  int sensorValue = digitalRead(sensorPin);
+  if(mqqtLastCount < count && millis() - last_mqtt_send_time >= mqtt_send_interval) {
+      preferences.putUInt("counter", count);
+      doc.clear();
+      doc["deviceId"] = deviceId;
+      doc["siteId"] = machineSiteId;
+      doc["count"] = count;
+      doc["publishedTime"] = mktime(&timeinfo);
+      doc["nodeMcuCode"] = String(deviceId)+"_"+machineSiteId;
+      
+      char mqtt_message[400];
+      serializeJson(doc, mqtt_message);
+      mqqtLastCount = count;
+      last_mqtt_send_time = millis();
+      delay(5000);
+      publishMessage(publishMsgTopicPrecede, mqtt_message, true);
+  }
+  
+}
+void publishMqttMessage(void * parameter) {
+  
+  while(true){
+
+    int sensorValue = digitalRead(sensorPin);
   
   if(sensorValue==HIGH  && lastSensorValue == LOW)
   {
@@ -187,64 +329,29 @@ void loop() {
 
     // Check if a minute has passed
 
-    // if (milli - lastMinuteMillis >= interval) {
-    //   // Calculate events per minute
-    //   float eventsPerMinute = count1 / ((milli - lastMinuteMillis) / 60000.0);
-    //   lcd.setCursor(7,0);
-    //   lcd.print("F");
-    //   lcd.setCursor(9,0);
-    //   lcd.print(count1);
-    //   feed = count1;
+    if (milli - lastMinuteMillis >= interval) {
+      // Calculate events per minute
+      float eventsPerMinute = count1 / ((milli - lastMinuteMillis) / 60000.0);
+      lcd.setCursor(7,0);
+      lcd.print("F");
+      lcd.setCursor(9,0);
+      lcd.print(eventsPerMinute);
+      feed = count1;
+      Serial.println("feed is ---------------------->"+ String(feed));
       
     // //   // Reset event count and update last minute time
-    //   count1 = 0;
-    //   lastMinuteMillis = milli;
-    // }
+      count1 = 0;
+      lastMinuteMillis = milli;
+    }
     
   }
   lastSensorValue = sensorValue;
-
   lcd.setCursor(0,1);
   lcd.print(count);
-    
-}
-
-
-
-void publishMqttMessage(void * parameter) {
-  while(true){
-
-    Serial.println(" WiFi status: " + String(WiFi.status()));
-    if (WiFi.status() != WL_CONNECTED) {
-      // lcd.clear();
-      wifiStatus = "WiFi NA";
-      Serial.println("Not connected to Wi-Fi. Reconnecting...");
-      setup_wifi(); // Reconnect to Wi-Fi
-      configTime(gmtOffset_sec, daylightOffset_sec, ntpServer); //configure time 
-      reconnect(); // Reconnect to MQTT broker
-    }
-    if(!getLocalTime(&timeinfo)){
-    Serial.println("Failed to obtain time");
-    return;
-    }
-
-    Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
-    Serial.println(" count from another thread is "+ String(count)+" mqqtLastCount is "+String(mqqtLastCount));
-    if(mqqtLastCount < count){
-      preferences.putUInt("counter", count);
-      doc.clear();
-      doc["deviceId"] = "nodemcu";
-      doc["siteId"] = machineSiteId;
-      doc["count"] = count;
-      doc["publishedTime"] = mktime(&timeinfo);
-      
-      char mqtt_message[400];
-      serializeJson(doc, mqtt_message);
-      mqqtLastCount = count;
-      publishMessage(machineTopic, mqtt_message, true);}
-    vTaskDelay(pdMS_TO_TICKS(mqtt_send_interval)); 
   }
 }
+
+
 
 
 
